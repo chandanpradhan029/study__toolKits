309decf08e20e796dc5e533f37ab6bb5f5e1bfc5
Chandan Pradhan <60406606+chandanpradhan029@users.noreply.github.com>
2022-09-19 22:04:44 +0530

    Update settings.py

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'student_kit',  
         'USER': 'root',  
         'PASSWORD': 'Root',  
-        'HOST': '127.0.0.1',  
+        'HOST': 'db',  
         'PORT': '3306',  
         'OPTIONS': {  
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" 
